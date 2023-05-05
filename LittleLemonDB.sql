e7b73b9c49a2e4d5c20d5d5dc9773ed05591ee8d
MiCaptain <lenix00@gmail.com>
2023-05-04 22:22:06 -0300

    Finalcommit

--- LittleLemonDB.sql
+++ LittleLemonDB.sql
@@ -300,3 +300,118 @@ CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY D
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+INSERT IGNORE INTO Bookings (BookingID, BookingSlot, EmployeeID, CustomersID, DeliveryID, OrderID, TableNO, BookingDate)
+VALUES
+(1, '19:00:00', 5, 9, 1, 1, 1, 2022-10-15),
+(2, '19:00:00', 8, 4, 2, 2, 2, 2022-10-15),
+(3, '15:00:00', 7, 8, 3, 5, 3, 2022-10-15),
+(4, '17:30:00', 7, 12, 4, 8, 4, 2022-10-15),
+(5, '18:30:00', 5, 7, 5, 3, 5, 2022-10-15),
+(6, '20:00:00', 8, 2, 6, 4, 6, 2022-10-15),
+(7, '18:00:00', 5, 7, 7, 6, 5, 2022-10-10),
+(8, '18:00:00', 5, 3, 8, 7, 3, 2022-10-12),
+(9, '17:00:00', 5, 9, 9, 9, 2, 2022-10-11),
+(10, '19:00:00', 5, 11, 10, 10, 2, 2022-10-13);
+
+INSERT IGNORE INTO MenuItems (ItemID, CourseName, StarterName, DesertName)
+VALUES
+(1, 'Salmon', 'Caesar Salad', 'Cheesecake'),
+(2, 'Beef Wellington', 'Caprese Salad', 'Chocolate Mousse'),
+(3, 'Lobster Bisque', 'Tuna Tartare', 'Crème Brûlée'),
+(4, 'Mushroom Risotto', 'Minestrone Soup', 'Tiramisu'),
+(5, 'Chicken Marsala', 'Bruschetta', 'Panna Cotta'),
+(6, 'Shrimp Scampi', 'House Salad', 'Gelato'),
+(7, 'Filet Mignon', 'French Onion Soup', 'Chocolate Fondant'),
+(8, 'Grilled Octopus', 'Greek Salad', 'Baklava'),
+(9, 'Lamb Chops', 'Hummus', 'Turkish Delight'),
+(10, 'Veal Parmesan', 'Antipasti Platter', 'Cannoli'),
+(11, 'Sushi Platter', 'Miso Soup', 'Green Tea Ice Cream'),
+(12, 'Crab Cakes', 'Seafood Chowder', 'Key Lime Pie'),
+(13, 'Pork Tenderloin', 'Arugula Salad', 'Chocolate Cake'),
+(14, 'Vegetable Curry', 'Naan Bread', 'Mango Sorbet');
+
+INSERT IGNORE INTO Menus (MenuID, ItemID, MenuName, Cuisine)
+VALUES
+(1, 1, 'Pacific Catch', 'Seafood'),
+(2, 2, 'English Countryside', 'British'),
+(3, 3, 'New England Feast', 'American'),
+(4, 4, 'Italian Delight', 'Italian'),
+(5, 5, 'Mediterranean Nights', 'Mediterranean'),
+(6, 6, 'Coastal Bliss', 'Seafood'),
+(7, 7, 'Classic Steakhouse', 'American'),
+(8, 8, 'Greek Isles', 'Greek'),
+(9, 9, 'Middle Eastern Journey', 'Middle Eastern'),
+(10, 10, 'Italian Feast', 'Italian'),
+(11, 11, 'Sushi Night', 'Japanese'),
+(12, 12, 'East Coast Seafood', 'Seafood'),
+(13, 13, 'Taste of the South', 'American'),
+(14, 14, 'Vegetarian Delight', 'Vegetarian');
+
+INSERT IGNORE INTO Customers (CustomersID, GuestFirstName, GuestLastName, Email, PhoneNumber)
+VALUES
+(1, 'Anna','Iversen','Anna.Iv@Livmail.com',984253698),
+(2, 'John', 'Doe', 'johndoe@example.com', 123456789),
+(3, 'Jane', 'Doe', 'janedoe@example.com', 234567890),
+(4, 'Alice', 'Smith', 'alicesmith@example.com', 345678901),
+(5, 'Bob', 'Smith', 'bobsmith@example.com', 456789012),
+(6, 'David', 'Lee', 'davidlee@example.com', 567890123),
+(7, 'Sarah', 'Kim', 'sarahkim@example.com', 678901234),
+(8, 'James', 'Johnson', 'jamesjohnson@example.com', 789012345),
+(9, 'Emily', 'Davis', 'emilydavis@example.com', 890123456),
+(10, 'William', 'Brown', 'williambrown@example.com', 901234567),
+(11, 'Olivia', 'Taylor', 'oliviataylor@example.com', 123456789),
+(12, 'Michael', 'Miller', 'michaelmiller@example.com', 234567890);
+
+INSERT IGNORE INTO Bookings (BookingID, BookingSlot, EmployeeID, CustomersID, DeliveryID, OrderID, TableNO, BookingDate)
+VALUES
+(1, '19:00:00', 5, 9, 1, 1, 1, 2022-10-15),
+(2, '19:00:00', 8, 4, 2, 2, 2, 2022-10-15),
+(3, '15:00:00', 7, 8, 3, 5, 3, 2022-10-15),
+(4, '17:30:00', 7, 12, 4, 8, 4, 2022-10-15),
+(5, '18:30:00', 5, 7, 5, 3, 5, 2022-10-15),
+(6, '20:00:00', 8, 2, 6, 4, 6, 2022-10-15),
+(7, '18:00:00', 5, 7, 7, 6, 5, 2022-10-10),
+(8, '18:00:00', 5, 3, 8, 7, 3, 2022-10-12),
+(9, '17:00:00', 5, 9, 9, 9, 2, 2022-10-11),
+(10, '19:00:00', 5, 11, 10, 10, 2, 2022-10-13);
+
+INSERT IGNORE INTO deliverystatus (DeliveryID, Status, Date)
+VALUES
+(1, 'Being prepared', '2022-10-15'),
+(2, 'Being prepared', '2022-10-15'),
+(3, 'Delivered', '2022-10-15'),
+(4, 'Delivered', '2022-10-15'),
+(5, 'Delivered', '2022-10-15'),
+(6, 'Waiting in line', '2022-10-15'),
+(7, 'Delivered', '2022-10-10'),
+(8, 'Delivered', '2022-10-12'),
+(9, 'Delivered', '2022-10-11'),
+(10, 'Delivered', '2022-10-13');
+
+INSERT IGNORE INTO Orders (OrderID, MenuID, BillAmount, Quantity)
+VALUES
+(1, 12, 25, 1),
+(2, 7, 40, 2),
+(3, 5, 30, 1),
+(4, 3, 225, 1),
+(5, 8, 45, 2),
+(6, 2, 15, 1),
+(7, 6, 35, 1),
+(8, 4, 175, 1),
+(9, 11, 50, 3),
+(10, 9, 25, 1),
+(11, 1, 60, 2),
+(12, 10, 40, 2);
+
+INSERT IGNORE INTO employees (EmployeeID, Name, Role, Address, Contact_Number, Email, Annual_Salary)
+VALUES
+(1,'Mario Gollini','Manager','724, Parsley Lane, Old Town, Chicago, IL',351258074,'Mario.g@littlelemon.com','$70,000'),
+(2,'Adrian Gollini','Assistant Manager','334, Dill Square, Lincoln Park, Chicago, IL',351474048,'Adrian.g@littlelemon.com','$65,000'),
+(3,'Giorgos Dioudis','Head Chef','879 Sage Street, West Loop, Chicago, IL',351970582,'Giorgos.d@littlelemon.com','$50,000'),
+(4,'Fatma Kaya','Assistant Chef','132 Bay Lane, Chicago, IL',351963569,'Fatma.k@littlelemon.com','$45,000'),
+(5,'Elena Salvai','Head Waiter','989 Thyme Square, EdgeWater, Chicago, IL',351074198,'Elena.s@littlelemon.com','$40,000'),
+(6,'John Millar','Receptionist','245 Dill Square, Lincoln Park, Chicago, IL',351584508,'John.m@littlelemon.com','$35,000'),
+(7,'Tom Smith','Waiter','111 Olive Ave, Lakeview, Chicago, IL',351753951,'Tom.s@littlelemon.com','$30,000'),
+(8,'Linda Brown','Waiter','432 Cherry St, River North, Chicago, IL',351852369,'Linda.b@littlelemon.com','$30,000');
--- runningsql.sql
+++ runningsql.sql
@@ -8,21 +8,6 @@ RIGHT JOIN menuitems ON menuitems.ItemID = menus.ItemID
 WHERE orders.BillAmount > 150
 ORDER BY orders.BillAmount ASC;
 
-SELECT * FROM customers;
-
-SELECT MenuName
-FROM menus
-WHERE MenuID = ANY (
-    SELECT MenuID
-    FROM orders
-    GROUP BY MenuID
-    HAVING SUM(Quantity) > 2
-)
-
-CREATE PROCEDURE GetMaxQuantity()
-SELECT MAX(quantity) AS "Max quantity in order" FROM orders;
-
-
 PREPARE GetOrderDetail FROM 
     'SELECT orders.orderID, orders.quantity, orders.BillAmount 
      FROM Orders
@@ -35,15 +20,8 @@ EXECUTE GetOrderDetail USING @id;
 
 SELECT * from bookings;
 
-CREATE PROCEDURE CheckBooking(IN bookingDate DATE, IN tableNo INT, OUT isBooked BOOLEAN)
-BEGIN
-    SET isBooked = FALSE;
-    SELECT COUNT(*) INTO isBooked FROM Bookings
-    WHERE TableNO = tableNo AND BookingDate = bookingDate;
-END
-
 call CheckBooking('19:00:00', 5, 2, '2022-10-19');
 call AddBooking(5, 2, '2022-10-20');
 call UpdateBooking(14, '2022-10-23');
 call CancelBooking(13);
-SELECT * FROM Bookings;
+SELECT * FROM orders;
